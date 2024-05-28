903894337496ab02ef5d4d830cb31b33dd31ea21
Andrej Korica <koricaandrej3@gmail.com>
2024-05-28 13:53:23 +0200

    ln and log now works on gui

--- src/ExpressionConverter.hs
+++ src/ExpressionConverter.hs
@@ -1,22 +1,26 @@
 module ExpressionConverter (tokenize) where
 
 import Data.Char (isDigit)
+import Debug.Trace (trace)
 
 -- Tokenize input string
 tokenize :: String -> [String]
 tokenize "" = []
 tokenize s@(c : cs)
-  | c `elem` " \t" = tokenize cs
-  | c `elem` "()+-*/^" = [c] : tokenize cs
+  | c `elem` " \t" = trace ("Tokenizing space/tab: " ++ [c]) $ tokenize cs
+  | c `elem` "()+-*/^" = trace ("Tokenizing operator: " ++ [c]) $ [c] : tokenize cs
+  | c == '(' =
+      let openParen = c : replicate 3 ' '
+          rest = dropWhile (`elem` " \t") cs
+       in trace ("Tokenizing open parenthesis: " ++ openParen) $ openParen : tokenize rest
+  | c == ')' = trace ("Tokenizing close parenthesis: " ++ [c]) $ [c] : tokenize cs
   | c == 'l' =
       let (funOperator, rest) = break (== '(') s
        in if take 3 funOperator == "log" || funOperator == "ln"
-            then funOperator : tokenize (drop (length funOperator) rest)
+            then let rest' = dropWhile (`elem` " \t") rest
+                 in trace ("Tokenizing function operator: " ++ funOperator) $ funOperator : tokenize rest'
             else error "Invalid token"
   | isDigit c || c == '.' =
       let (num, rest) = span (\x -> isDigit x || x == '.') s
-       in num : tokenize rest
+       in trace ("Tokenizing number: " ++ num) $ num : tokenize rest
   | otherwise = error "Invalid token"
-
-  
--- TODO: Add function to convert expression to a format suitable for infix to postfix conversion
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,25 @@ module Main (main) where
 
 import Graphics.Gloss.Interface.IO.Game (playIO, white, Display(..))
 import CalculatorGUI (render, handleEvent, update, initialState)
+import InfixPostfix (simSYA, evalPostfix)
+import ExpressionConverter (tokenize)
+import Utils (fst3, snd3)
+import Text.Printf (printf)
+
+-- Define trd3 utility function
+trd3 :: (a, b, c) -> c
+trd3 (_, _, z) = z
 
 main :: IO ()
-main = playIO (InWindow "Scientific calculator" (400, 550) (10, 10)) white 60 initialState render handleEvent update
+main = do
+  let expression = "log5(5 + 2)"
+      tokens = tokenize expression
+      result = last $ simSYA tokens
+      finalPostfix = reverse $ fst3 result
+      finalResult = unwords finalPostfix
+      evaluatedResult = evalPostfix finalPostfix
+  printf "Postfix Expression: %s\n" finalResult
+  printf "Evaluated Result: %.2f\n" evaluatedResult
+
+  -- Start the graphical interface
+  playIO (InWindow "Scientific calculator" (400, 550) (10, 10)) white 60 initialState render handleEvent update
